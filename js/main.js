bacf1f74a026c59795849569db2c6acf87ca9e10
Elisa Silvestrini <eli.silve215@gmail.com>
2026-05-05 22:44:14 +0200

    style: :lipstick: customize header

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    loadHeader();
+});
+
+function loadHeader() {
+    const $headerPlaceholder = $("#header");
+
+    if (!$headerPlaceholder.length) return;
+
+    $headerPlaceholder.load("components/header.html", function () {
+        setActiveNavLink();
+        initMobileMenu();
+        initHeaderOnScroll();
+    });
+}
+
+function setActiveNavLink() {
+    const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+    $(".header-nav a").each(function () {
+        const linkPage = $(this).attr("href");
+
+        if (linkPage === currentPage) {
+            $(this).addClass("active");
+        }
+    });
+}
+
+function initMobileMenu() {
+    const $toggle = $(".menu-toggle");
+    const $nav = $(".header-nav");
+
+    if (!$toggle.length || !$nav.length) return;
+
+    $toggle.on("click", function () {
+        $(this).toggleClass("active");
+        $nav.toggleClass("open");
+        $("body").toggleClass("menu-open");
+    });
+}
+
+function initHeaderOnScroll() {
+    const $header = $(".site-header");
+
+    if (!$header.length) return;
+
+    $(window).on("scroll", function () {
+        $header.toggleClass("scrolled", $(window).scrollTop() > 20);
+    });
+}
